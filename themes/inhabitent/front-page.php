76d930bd76de73ea262610ff9394ad7981c3d0e5
kaveh1987 <you@email.com>
2016-08-12 14:04:51 -0700

    first commit

---
+++ themes/inhabitent/front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+<h1>Hello world</h1>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
